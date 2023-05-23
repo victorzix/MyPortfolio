33076575eec934b4edb59fad8966e3604fd84cac
victorzix <89355801+victorzix@users.noreply.github.com>
2023-05-23 13:14:08 -0300

    Font update globalStyles.jsx

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -3,12 +3,11 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 *{
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400;500;600;700&display=swap');
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Montserrat', sans-serif;
-
+  font-family: 'Roboto Mono', monospace;
 }
 body{
   padding: 3em 8em 0 8em;
