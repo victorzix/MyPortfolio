344ac2d9d1ff67892a1663ccc81161cb02955f81
victorzix <linkgamesplays@gmail.com>
2023-06-18 23:38:35 -0300

    style: style scrollbar

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,15 +1,42 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap');
+
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
+
+    ::-webkit-scrollbar{
+      width: .6em;
+    }
+
+    ::-webkit-scrollbar-track{
+      background-color: var(--dark-blue);
+    }
+
+    ::-webkit-scrollbar-thumb{
+     background-color: var(--ice-white);
+     border-radius: 2em;
+     border: .20em solid var(--dark-blue);
+
+     &:hover{
+      background-color: var(--txt-yellow);
+     }
+    }
+
+    ::-webkit-scrollbar-thumb:hover{
+      
+
+    }
+
+    
 }
   body {
     height: 100vh;
     width: auto;
-    background-color: ;
   }
 
   @media(max-width: 400px){
