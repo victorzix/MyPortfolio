dbc92a8cf0abbf4b5dad34ae1c36376ff1e90780
victorzix <linkgamesplays@gmail.com>
2023-06-18 23:42:05 -0300

    feat: add components to app and globalstyle to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import Footer from "./components/Footer";
-import AboutMe from "./pages/aboutMe/AboutMe";
-import MainProjects from "./pages/mainProjects/MainProjects";
-import Projects from "./pages/projects/Projects";
-import { scrollSetup } from "./utils/scrollSetup";
-import { useEffect } from "react";
-import Button from './components/TopBtn'
-
-
-
+import Header from "./components/Header"
+import Intro from "./components/Intro"
+import AboutMe from "./components/AboutMe"
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
 
 function App() {
 
-  useEffect(() => {
-    scrollSetup();
-  });
   return (
     <>
-        <AboutMe/>
-        <MainProjects/>
-        <Projects/>
-        <Footer/>
-
-        <Button/>
+    <Header/>     
+    <Intro/>
+    <AboutMe/>
+    <Skills/>
+    <Projects/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import GlobalStyle from './styles/globalStyles'
+import App from './App.jsx'
+import GlobalStyle from './GlobalStyle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
